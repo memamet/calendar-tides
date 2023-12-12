59d959cc66cfb5ef3bea78a300886c72ab714e09
Max-Eugène Mamet <max-eugene@hotmail.fr>
2023-12-12 15:46:22 +0100

    --wip-- [skip ci]

---
+++ src/app/components/TideCalendar.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+
+const TideCalendar = () => {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+};
+
+export default TideCalendar;
